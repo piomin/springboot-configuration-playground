8e800d3ae73c3ece4c9a2393716e186a14e481fc
Piotr Mińkowski <piotr_minkowski@wp.pl>
2019-03-10 00:38:51 +0100

    adding configuration properties sample test

---
+++ src/main/java/pl/piomin/services/playground/model/PersonsList.java
@@ -0,0 +1,23 @@
+package pl.piomin.services.playground.model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@ConfigurationProperties("persons-list")
+public class PersonsList {
+
+    private List<Person> persons = new ArrayList<>();
+
+    public List<Person> getPersons() {
+        return persons;
+    }
+
+    public void setPersons(List<Person> persons) {
+        this.persons = persons;
+    }
+
+}
---
+++ src/test/java/pl/piomin/services/playground/ConfigurationPropertiesTest.java
@@ -0,0 +1,26 @@
+package pl.piomin.services.playground;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.piomin.services.playground.model.PersonsList;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class ConfigurationPropertiesTest {
+
+    @Autowired
+    PersonsList personsList;
+
+    @Test
+    public void test() {
+        Assert.assertTrue(personsList.getPersons().size() == 3);
+        Assert.assertEquals("John", personsList.getPersons().get(0).getFirstName());
+        Assert.assertEquals("Tom", personsList.getPersons().get(1).getFirstName());
+        Assert.assertEquals("Kate", personsList.getPersons().get(2).getFirstName());
+    }
+
+}
