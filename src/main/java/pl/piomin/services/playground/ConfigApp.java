ac8a941b3dd2dcf36807d13312502a5c971f038c
piomin <piotr.minkowski@gmail.com>
2022-08-01 16:57:39 +0200

    Add another property for testing on k8s

--- src/main/java/pl/piomin/services/playground/ConfigApp.java
+++ src/main/java/pl/piomin/services/playground/ConfigApp.java
@@ -1,21 +1,33 @@
 package pl.piomin.services.playground;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
 
 @SpringBootApplication
 @PropertySource(value = "classpath:/additional.yml", ignoreResourceNotFound = true)
 public class ConfigApp {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ConfigApp.class);
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(ConfigApp.class);
-        app.setDefaultProperties(Map.of("property1", "default"));
+        app.setDefaultProperties(Map.of("property.default", "default"));
         app.setAllowBeanDefinitionOverriding(true);
         app.run(args);
-//        SpringApplication.run(ConfigApp.class, args);
     }
 
+    @Value("${property.default}")
+    String propertyDefault;
+
+    @PostConstruct
+    public void printInfo() {
+        LOG.info("========= Property (default): {}", propertyDefault);
+    }
 }
