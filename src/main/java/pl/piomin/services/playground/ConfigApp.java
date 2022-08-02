8a6431def7cae0e6a5a9e5ecb6afca1db26875f9
piomin <piotr.minkowski@gmail.com>
2022-08-02 10:02:39 +0200

    Adding Kubernetes-specific configuration

--- src/main/java/pl/piomin/services/playground/ConfigApp.java
+++ src/main/java/pl/piomin/services/playground/ConfigApp.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.cloud.CloudPlatform;
 import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
@@ -25,9 +26,12 @@ public class ConfigApp {
 
     @Value("${property.default}")
     String propertyDefault;
+    @Value("${property.activation:none}")
+    String propertyActivation;
 
     @PostConstruct
     public void printInfo() {
         LOG.info("========= Property (default): {}", propertyDefault);
+        LOG.info("========= Property (activation): {}", propertyActivation);
     }
 }
